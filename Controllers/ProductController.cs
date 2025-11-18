6e70cc1071c6ad85feec90e821c4a84e055eaaef
anil kumar <anilthirumala@live.com>
2025-11-18 11:55:41 +0530

    initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestApp.Models;
+using TestApp.Services;
+namespace TestApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _service= new ProductService();
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View(_service.GetProducts);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product collection)
+        {//Product p=new Product();
+            //p.Display();
+            try
+            {
+                _service.AddProduct(collection);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Services/PrdouctService.cs
@@ -0,0 +1,14 @@
+﻿using TestApp.Models;
+
+namespace TestApp.Services
+{
+    public class ProductService
+    {
+        public static List<Product> products = new List<Product>();
+        public List<Product> GetProducts => products;
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+    }
+}
